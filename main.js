1ef2f381c878a2fe0ce524f49b29466454bd9c6c
aylen luquez <aylen968@gmail.com>
2023-09-19 22:25:47 -0300

    3rd day

---
+++ main.js
@@ -0,0 +1,19 @@
+const itemsContainer = document.getElementById("items");// tomo elementos del div con id="items"
+const itemTemplate = document.getElementById("template"); // lo mismo con id="template"
+const addButton = document.getElementById("add")// lo mismo con id="add"
+
+//now i should keep going on how to retrieve data from local storage to keep the list items
+
+
+
+
+
+
+
+
+
+
+
+
+
+
